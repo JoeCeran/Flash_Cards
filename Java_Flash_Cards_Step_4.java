b0e4bfbcb3b6455e647d29c79666d99c4e2acfa7
Joseph Cerankowski <Joecerankowski@gmail.com>
2019-09-24 10:31:44 -0400

    Update Java_Flash_Cards_Step_4.java

--- Java_Flash_Cards_Step_4.java
+++ Java_Flash_Cards_Step_4.java
@@ -7,66 +7,61 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         boolean exists = true;
         Scanner scanner = new Scanner(System.in);
         LinkedHashMap<String, String> cardToDefinition = new LinkedHashMap<>();
-        LinkedHashMap<String, String> DefinitonToCard = new LinkedHashMap<>();
+        LinkedHashMap<String, String> definitionToCard = new LinkedHashMap<>();
         System.out.println("Input the number of cards:");
         int carded = scanner.nextInt();
+        String key = "";
         scanner.nextLine();
 
         for (int i = 0; i < carded; i++) {
 
-
             System.out.println("The card #" + (i + 1));
             String cardedd = scanner.nextLine();
-            for (String j : cardToDefinition.keySet()) {
-                if (cardedd.equals(j)) {
-                    do {
-                        System.out.println("The card \"" + cardedd + "\" already exists. Try again:");
-                        cardedd = scanner.nextLine();
-                        if (cardedd.equals(j)) {
-                            exists = true;
-                        } else {
-                            exists = false;
-                        }
-                    } while (exists == true);
-                }
+            if (cardToDefinition.containsKey(cardedd)) {
+                do {
+                    System.out.println("The card \"" + cardedd + "\" already exists. Try again:");
+                    cardedd = scanner.nextLine();
+                    if (cardToDefinition.containsKey(cardedd)) {
+                        exists = true;
+                    } else {
+                        exists = false;
+                    }
+                } while (exists == true);
             }
 
             System.out.println("The definition of the card #" + (i + 1));
             String definition = scanner.nextLine();
-            for (String j : cardToDefinition.values()) {
-                if (definition.equals(j)) {
-                    do {
-                        System.out.println("The definition \"" + definition + "\" already exists. Try again:");
-                        definition = scanner.nextLine();
-                        if (definition.equals(j)) {
-                            exists = true;
-                        } else {
-                            exists = false;
-                        }
-                    } while (exists == true);
-                }
+            if (cardToDefinition.containsValue(definition)) {
+                do {
+                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
+                    definition= scanner.nextLine();
+                    if (cardToDefinition.containsValue(definition)) {
+                        exists = true;
+                    } else {
+                        exists = false;
+                    }
+                } while (exists == true);
             }
             cardToDefinition.put(cardedd, definition);
+            definitionToCard.put(definition, cardedd);
         }
-
+            exists = true;
             for (String j : cardToDefinition.keySet()) {
                 System.out.println("Print the definition of \"" + j + "\":");
                 String input = scanner.nextLine();
-                //for (String j : cardToDefinition.keySet()) {
                 if (cardToDefinition.get(j).equals(input)) {
                     System.out.println("Correct answer");
                 }
-//                else if (input.equals(j)) {
-//                    System.out.println("Wrong answer. the correct one is \"" + cardToDefinition.get(i) + "\", you've just written the definition of \"" + input + "\".");
-//                }
+                else if (!input.equals(j) && cardToDefinition.containsValue(input) ) {
+                        System.out.println("Wrong answer. the correct one is \"" + cardToDefinition.get(j) + "\", you've just written the definition of \"" + definitionToCard.get(input) + "\".");
+                }
                 else {
                     System.out.println("Wrong answer. the correct one is \"" + cardToDefinition.get(j) + "\".");
                 }
-                //}
             }
         }
     }
